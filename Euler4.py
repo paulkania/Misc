15cc6b38c1ead1bf67be0a1116d8e17e9a2a9827
PaulKania <p.rozehnal@gmail.com>
2019-11-28 14:34:37 -0500

    Palindrome - New tech using iter.permu

--- Euler4.py
+++ Euler4.py
@@ -1,11 +1,21 @@
-z=[]
-for i in range(100,1000):
-    for j in range (100,1000):
-        z.append(i*j)
+import time as t
+import itertools as iter
+bt= t.time()
 
-z=sorted(set(z))
 
-z= [str(i) for i in z]
-z= [int(i) for i in z if i== i[::-1]]
-z=max(z)
-print(z)
+#------------
+def paully_the_pally_finder(int1,int2,num_of_products=2):
+    l=[]
+    a=list(iter.permutations(range(int1,int2,-1),num_of_products))
+    for i in a:
+        s= str(i[0]*i[1])
+        if s == s[::-1]:
+            l.append(s)
+        if len(l)>30:
+            print(sorted(l),i[0],i[1])
+            break
+#-------------
+    
+paully_the_pally_finder(1000,317,2)
+ft= t.time()
+print(round((ft-bt),5))
